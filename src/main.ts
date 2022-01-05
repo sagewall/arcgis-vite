574a05a53af70cdfc0a295cc16207ccb1fc9f6f9
Sage Wall <sagewall@users.noreply.github.com>
2022-01-05 09:53:38 -0700

    ⬆️ Upgrade dependencies 20220105

--- src/main.ts
+++ src/main.ts
@@ -17,10 +17,10 @@ import './style.css'
 
 esriConfig.apiKey = import.meta.env.VITE_ARCGIS_API_KEY as string
 esriConfig.assetsPath =
-  'https://cdn.jsdelivr.net/npm/@arcgis/core@4.21.2/assets'
+  'https://cdn.jsdelivr.net/npm/@arcgis/core@4.22.2/assets'
 
 setAssetPath(
-  'https://cdn.jsdelivr.net/npm/@esri/calcite-components@1.0.0-beta.71/dist/calcite/assets'
+  'https://cdn.jsdelivr.net/npm/@esri/calcite-components@1.0.0-beta.73/dist/calcite/assets'
 )
 defineCustomElements()
 
