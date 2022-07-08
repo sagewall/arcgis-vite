96fd5474b706d029e3c3ca6373d8fed5c12a45ff
Sage Wall <swall@esri.com>
2022-07-08 13:07:12 -0600

    ⬆️ Upgraded @arcgis/core to 4.25.0-next.20220708

--- src/main.ts
+++ src/main.ts
@@ -16,8 +16,6 @@ import {
 import './style.css'
 
 esriConfig.apiKey = import.meta.env.VITE_ARCGIS_API_KEY as string
-esriConfig.assetsPath =
-  'https://cdn.jsdelivr.net/npm/@arcgis/core@4.24.6/assets'
 
 setAssetPath(
   'https://cdn.jsdelivr.net/npm/@esri/calcite-components@1.0.0-beta.83/dist/calcite/assets'
